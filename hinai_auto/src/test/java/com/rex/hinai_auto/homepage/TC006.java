528c5e5627d70273128c9672779d3d031134ce4a
errashmi <errashmiranjansahoo@gmail.com>
2020-01-26 19:01:54 +0530

    Test6 added

---
+++ hinai_auto/src/test/java/com/rex/hinai_auto/homepage/TC006.java
@@ -0,0 +1,14 @@
+package com.rex.hinai_auto.homepage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class TC004 {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
